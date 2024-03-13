b50adf23ee5fc006d6512212d19c5e4da27f9bd7
thepi <thepig6704@gmail.com>
2024-03-13 13:33:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayoutManager mLinearLayoutManager;
+    RecyclerView mRecyclerView;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mDatabaseReference;
+    FirebaseRecyclerAdapter<Model,ViewHolder> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Model>options;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLinearLayoutManager=new LinearLayoutManager(this);
+        mLinearLayoutManager.setReverseLayout(true);
+        mLinearLayoutManager.setStackFromEnd(true);
+        mRecyclerView=findViewById(R.id.recyclerView);
+        mFirebaseDatabase=FirebaseDatabase.getInstance();
+        mDatabaseReference= mFirebaseDatabase.getReference("Data");
+        showData();
+    }
+
+    private void showData() {
+        options = new FirebaseRecyclerOptions.Builder<Model>().setQuery(mDatabaseReference,Model.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Model, ViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Model model) {
+                holder.setDetails(getApplicationContext(),model.getTitle(), model.getImage(), model.getAuthor());
+            }
+
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView= LayoutInflater.from(parent.getContext()).inflate(R.layout.row,parent,false);
+                ViewHolder viewHolder=new ViewHolder(itemView);
+                viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                        Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return viewHolder;
+            }
+        };
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        firebaseRecyclerAdapter.startListening();
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(firebaseRecyclerAdapter!=null){
+            firebaseRecyclerAdapter.startListening();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/demo/Model.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+public class Model {
+    String title;
+    String image;
+    String author;
+
+    public Model() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
